ded1278825ef1c6e995e700e94d456e3119ab073
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-01-23 00:59:53 +0100

    Transfer data

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class FirstComponentComponent {
 
+  name = "Hamza Haj Mtir";
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 }
