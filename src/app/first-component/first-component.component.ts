a582bdd2f1a5b2816f117620532be932e05f46bc
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-01-23 18:05:27 +0100

    course changes

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-first-component',
   standalone: true,
@@ -8,14 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
-
-  name = "Hamza Haj Mtir";
-  constructor(){
-
-  }
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+Onchange(arg0: any) {
+throw new Error('Method not implemented.');
+}
+    name = "Hamza Haj Mtir";
+    age = "21";
+    imageURL = "https://image.elite-auto.fr//visuel/AUDI/audi_22rs7sprtbk4wdhb2b_angularfront.png";
     
-  }
+    job = "";
+    
+    change(){
+      this.name="Bouhmez";
+      this.age="50";
+    }
+
+    OnChange(value: string){
+      this.job =value;
+    }
+    constructor(){
+      
+    }
+    ngOnInit(): void {
+      //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+      //Add 'implements OnInit' to the class.
+      
+    }
 }
